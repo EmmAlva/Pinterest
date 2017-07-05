29bb6e3954549833a3e98b31a0070869f057eed3
cindy <mendozaic@hotmail.com>
2017-07-04 19:07:32 -0500

    mas header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,59 @@
+'use strict'
 
+const render = (root) => {
+  root.empty();
+  const wrapper = $('<div class="wrapper"></div>');
+  switch (state.selectedPinterest) {
+    case null:
+      wrapper.append(Header(_ => render(root)));
+      root.append(wrapper);
+      break;
+
+    // case 1:
+    //   wrapper.append(validNumber(_ => render(root)));
+    //   root.append(wrapper);
+    //   break;
+    //
+    // case 2:
+    //   wrapper.append(enterYourCode(_ => render(root)));
+    //   root.append(wrapper);
+    //   break;
+    //
+    // case 3:
+    //   wrapper.append(createYourUser(_ => render(root)));
+    //   root.append(wrapper);
+    //   break;
+    //
+    // case 4:
+    //   wrapper.append(youCanUse(_ => render(root)));
+    //   root.append(wrapper);
+    //   break;
+    //
+    // case 5:
+    //   wrapper.append(registerYourDebitCard(_ => render(root)));
+    //   root.append(wrapper);
+    //   break;
+    //
+    // case 6:
+    //   wrapper.append(enterYourPasswordCard(_ => render(root)));
+    //   root.append(wrapper);
+    //   break;
+    //
+    // case 7:
+    //   wrapper.append(lastMoves());
+    //   root.append(wrapper);
+    //   break;
+    //
+    // default:
+
+  }
+}
+
+const state = {
+  selectedPinterest: null
+};
+
+$( document ).ready(function() {
+  const root = $('.root');
+  render(root);
+});
---
+++ src/assets/js/componentes/header.js
@@ -0,0 +1,53 @@
+const Header = (update) =>{
+   const header = $("<header></header>");
+   const section = $("<section></section>");
+   const divContainerfluid = $("<div class='container-fluid'></div>");
+   const divRow = $("<div class='row flex-container-row margin1x1'></div>");
+
+   const divCol1 = $("<div class='col-xs-3 col-md-2'></div>");
+   const aPintLog = $("<a href='#'></a>");
+   const divPintLogHover = $("<div class='hover flex-container-column'></div>");
+   const imgPintLog = $("<img src='pinterest-logo.png' height='28px' width='28px'>");
+   const pPintLog = $("<p class='darkslategray-text-color'>Inicio</p>");
+
+   const divCol2 = $("<div class='visible-xs col-xs-3'></div>");
+   const divIcoSearchHover = $("<div class='hover flex-container-column'></div>");
+   const icoSearch = $("<i style='font-size:24px' class='glyphicon glyphicon-search silver-text-color'></i>");
+   const pIcoSeaerch = $("<p class='silver-text-color'>Buscar</p>");
+
+   const divCol3 = $("<div class='hidden-xs col-md-4'></div>");
+   const divInputGroup = $("<div class='input-group'></div>");
+   const spanInpAdd = $("<span class='input-group-addon'></span>");
+   const icoSearchHidden = $("<i style='font-size:22px' class='glyphicon glyphicon-search silver-text-color'></i>");
+   const inputHiddenSearch = $("<input type='text' class='form-control' placeholder='Buscar'>");
+
+   const divCol4 = $("<div class='col-xs-2 col-md-2'></div>");
+   const aUser = $("<a href='#'></a>");
+   const divUserHov = $("");
+
+   icoSearchHidden.append(spanInpAdd);
+   spanInpAdd.append(divInputGroup);
+   inputHiddenSearch.append(divInputGroup);
+   divInputGroup.append(divCol3);
+   divCol3.append(divContainerfluid);
+
+   icoSearch.append(divIcoSearchHover);
+   pIcoSeaerch.append(divIcoSearchHover);
+   divIcoSearchHover.append(divCol2);
+   divCol2.append(divContainerfluid);
+
+   imgPintLog.append(divPintLogHover);
+   pPintLog.append(divPintLogHover);
+   divPintLogHover.append(aPintLog);
+   aPintLog.append(divCol1);
+   divCol1.append(divRow);
+
+   divRow.append(divContainerfluid);
+   divContainerfluid.append(section);
+   section.append(header);
+  //  bntSignUp.on("click",function(event) {
+  //   state.selectedYape = 1;
+  //   update();
+  //  })
+   return header;
+ }
